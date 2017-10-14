090c91748ad9535fd457e3854b3ed922752f25bc
gokkulasudanr92 <gokkulasudanr92@gmail.com>
2017-10-14 19:50:56 -0400

    Index page to capture camera images

--- django-webapp/kanjoos/kanjoos/urls.py
+++ django-webapp/kanjoos/kanjoos/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^index/$', myapp_views.index, name='index'),
+    url(r'^home$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', myapp_views.index, name='index'),
     url(r'^api/v1/courses', include('myapp.urls', namespace='courses')),
     url(r'^api/v1/images/', include('myapp.urls', namespace='images')),
 ]
