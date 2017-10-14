33cb7a88ca140ce00fca8c46548f119112208f59
gokkulasudanr92 <gokkulasudanr92@gmail.com>
2017-10-14 06:26:19 -0400

    Basic Angular JS integrated with Django Framework

--- django-webapp/kanjoos/kanjoos/urls.py
+++ django-webapp/kanjoos/kanjoos/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
-from myapp.views import HomeView
+from myapp import views as myapp_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/',include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^$', HomeView.as_view()), 
+    url(r'^$', myapp_views.home, name='home'),
 ]
--- django-webapp/kanjoos/myapp/views.py
+++ django-webapp/kanjoos/myapp/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.conf import settings
 
-class HomeView(TemplateView):
-    template_name = 'index.html'
+def home(req):
+    return render(req, 'index.html', {'STATIC_URL': settings.STATIC_URL})
