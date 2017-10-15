741935aea335271cf8789e79a74af92f74adde01
vijayaganesh <mvijayaganesh94@gmail.com>
2017-10-15 03:23:14 -0400

    Backend integrated with Django

--- django-webapp/kanjoos/myapp/src/runner/Request.py
+++ django-webapp/kanjoos/myapp/src/runner/Request.py
@@ -12,6 +12,9 @@ __status__      = "Development"
 import cv2
 import numpy as np
 import json
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__),'..','helper'))
+import utilities
 
 class Request(object):
     """ Request class has the following attributes:
@@ -24,14 +27,16 @@ class Request(object):
             generate_response() : Method to use annotating functions to generate the JSON response
     """
 
-
     def __init__(self,img_bytearray):
         self.img_bytearray = img_bytearray
-        image = cv2.decode(np.fromstring(img_bytearray,np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
-        response = self.generate_response()
+        self.image = cv2.imdecode(np.fromstring(img_bytearray,np.uint8),cv2.IMREAD_UNCHANGED)
+        print(self.image.shape)
+        # print('Response Initialized')
+        self.response = self.generate_response()
 
     def generate_response(self):
         logo_annotation_tf = utilities.predict_tf_logo(self.image)
+        print(logo_annotation_tf)
         logo_annotation_vision = utilities.predict_vision_logo(self.img_bytearray)
         label_annotation_vision = utilities.predict_vision_label(self.img_bytearray)
         text_annotation_vision = utilities.predict_vision_text(self.img_bytearray)
@@ -39,16 +44,15 @@ class Request(object):
         response = {}
         if (logo_annotation_vision == None):
             response['logo'] = logo_annotation_tf
-        else
-            response['logo'] = logo_annnotation_vision
-        label_set = set()
-        for labels in label_annotation_tf:
-            label_set.add(labels)
-        for labels in label_annotation_vision:
-            label_set.add(labels)
+        else:
+            response['logo'] = logo_annotation_vision
+        # label_set = set()
+        # for labels in label_annotation_tf:
+        #     label_set.add(labels)
+        # for labels in label_annotation_vision:
+        #     label_set.add(labels)
         response['label'] = label_annotation_vision
         response['text'] = text_annotation_vision
         response['color'] = color_annotation_vision
-        json_response = json.dump(response)
-        print(json_response)
-        return json_response
+        # print(response)
+        return response
--- django-webapp/kanjoos/myapp/views.py
+++ django-webapp/kanjoos/myapp/views.py
@@ -10,10 +10,22 @@ from rest_framework import status
 from . import models
 from . import serializers
 
-# Add Imports here 
+import io,sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__),'src','helper'))
+print(os.path.join(os.path.dirname(__file__),'src','helper'))
+import utilities
+sys.path.append(os.path.join(os.path.dirname(__file__),'src','runner'))
+from Request import Request
+sys.path.append(os.path.join(os.path.dirname(__file__),'src','constants'))
+import Constant
+import cv2
+import numpy as np
+import base64
+
+# Add Imports here
 
 def index(req):
-    return render(req, 'index.html', {'STATIC_URL': settings.STATIC_URL})
+	return render(req, 'index.html', {'STATIC_URL': settings.STATIC_URL})
 
 class ListCreateCourse(APIView):
 	def get(self, request, format=None):
@@ -31,9 +43,34 @@ def list(req):
 	# Logic to get list of the data
 	# Integrate the JSON here
 	images = models.ImageInfo.objects.all()
+	print(type(images))
 	for image in images:
+	    # print(type(image.imageUri))
+	    image_string = base64.b64decode(image.imageUri[23:])
+	    # print(image_string)
+	    request = Request(image_string)
+	    image.logo = request.response['logo']
+	    image.label = request.response['label']
+	    image.text = request.response['text']
+	    image.color = request.response['color']
+
+	    image.save()
 		# For image format refer in models.py file in myapp folder
-	return render(req, 'list.html', {'STATIC_URL': settings.STATIC_URL})
+	list = models.ImageInfo.objects.all()
+	'''result = []
+	for item in list:
+	    temp = {};
+	    temp["id"] = item.id
+	    temp["logo"] = item.logo
+	    temp["text"] = item.text
+	    temp["color"] = item.color
+	    result.append(temp);'''
+
+	context={
+	  'result': list,
+	  'STATIC_URL': settings.STATIC_URL
+	}
+	return render(req, 'list.html', context)
 
 class ListCreateImageInfo(APIView):
 	@csrf_exempt
